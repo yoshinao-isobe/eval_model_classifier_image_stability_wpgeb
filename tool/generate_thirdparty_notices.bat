5dda83599befefa2fde22b1aa7d76d6a5c715d30
kinkan.ri <kinkan.ri@aist.go.jp>
2023-09-27 11:38:33 +0900

    Initialization

--- tool/generate_thirdparty_notices.bat
+++ tool/generate_thirdparty_notices.bat
@@ -3,7 +3,6 @@
 rem #####AIT Dockerイメージ作成#####
 
 set DOCKER_IMAGE_NAME=ait_license_base:latest
-set REGISTORY=localhost:5500
 
 cd /d %~dp0
 
@@ -11,19 +10,10 @@ rem 既存削除
 echo start docker crean up...
 docker rmi %DOCKER_IMAGE_NAME%
 docker system prune -f
-docker rmi %REGISTORY%/%DOCKER_IMAGE_NAME%
 
 rem ビルド
 echo start docker build...
 docker build -f ..\deploy\container\dockerfile -t %DOCKER_IMAGE_NAME% ..\deploy\container
-
-rem タグ付け
-echo start docker tag...
-docker tag %DOCKER_IMAGE_NAME% %REGISTORY%/%DOCKER_IMAGE_NAME%
-
-rem プッシュ
-echo start docker push...
-docker push %REGISTORY%/%DOCKER_IMAGE_NAME%
 
 rem #####AITライセンス情報出力#####
 
